9d009970745751c74e58f80358a3c34378548889
Corby Shaner <corbyshaner@gmail.com>
2019-11-04 08:07:39 -0500

    Added more modules.

---
+++ threads/src/main.rs
@@ -0,0 +1,131 @@
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    {
+        // Basic thread
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        handle.join().unwrap();
+
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    }
+
+    {
+        // 1 Thread with channel
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let val = String::from("hi");
+            tx.send(val).unwrap();
+        });
+
+        let received = rx.recv().unwrap();
+        println!("Got: {}", received);
+    }
+
+    {
+        // 1 Thread with multiple values, pausing between each
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("1 thread, mult vals Got: {}", received);
+        }
+    }
+
+    {
+        // 2 threads, 1 channel
+        let (tx, rx) = mpsc::channel();
+
+        let tx1 = mpsc::Sender::clone(&tx);
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("Got: {}", received);
+        }
+    }
+
+    {
+        // Mutex in single thread
+        let m = Mutex::new(5);
+
+        {
+            let mut num = m.lock().unwrap();
+            *num = 6;
+        }
+
+        println!("m = {:?}", m);
+    }
+
+    {
+        // Mutex on multiple threads
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
+    }
+}
